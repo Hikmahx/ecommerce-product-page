f87a6509e2f1fd0ae2c59f984fb6554ce41729f2
Hikmahx <hikmayousuph@gmail.com>
2022-02-19 12:51:46 +0100

    images state added to context

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,10 +1,19 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
+import imgProduct1 from '../assets/image-product-1.jpg'
+import imgProduct2 from '../assets/image-product-2.jpg'
+import imgProduct3 from '../assets/image-product-3.jpg'
+import imgProduct4 from '../assets/image-product-4.jpg'
 
 const ProductContext = createContext()
 
 export const ProductProvider = ({children})=>{
+
+  const [images, setImages] = useState([imgProduct1, imgProduct2, imgProduct3, imgProduct4])
+
   return (
-    <ProductContext.Provider value={{}}>
+    <ProductContext.Provider value={{
+      images
+    }}>
       {children}
     </ProductContext.Provider>
   )
