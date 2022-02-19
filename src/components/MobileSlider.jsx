beafdb0c7854ecfd3f4e3460ba8296b360d3f5d0
Hikmahx <hikmayousuph@gmail.com>
2022-02-19 12:52:34 +0100

    mobile and tablet view completed

--- src/components/MobileSlider.jsx
+++ src/components/MobileSlider.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ProductContext from '../context/ProductContext'
 
 const MobileSlider = () => {
+
+  const {images} = useContext(ProductContext)
+
   return (
-    <div></div>
+    <div className="slider overflow-hidden relative mt-1">
+      <div className="images flex w-full sm:w-1/2">
+        {images.map(image=>(
+          <img src={image} alt="slider-img" />
+        ))}
+      </div>
+      <div className="directions absolute inset-x-0 inset-y-1/2 flex justify-between mx-4">
+        <button className="back-arrow w-10 h-10 bg-white rounded-full">
+          <i className="flex items-center justify-center m-auto text-lg">
+            <ion-icon name="chevron-back-outline"></ion-icon>
+          </i>
+        </button>
+        <button className="next-arrow w-10 h-10 bg-white rounded-full">
+          <i className="flex items-center justify-center m-auto text-lg">
+            <ion-icon name="chevron-forward-outline"></ion-icon>
+          </i>
+        </button>
+      </div>
+    </div>
   )
 }
 
