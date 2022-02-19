b962f1b295a48ca457768d85c2e7a9cd96fc5329
Hikmahx <hikmayousuph@gmail.com>
2022-02-19 13:40:18 +0100

    colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
       'orange': 'hsl(26, 100%, 55%)',
       'pale-orange': 'hsl(25, 100%, 94%)',
       'very-dark-blue': 'hsl(220, 13%, 13%)',
-      'dark-grayish blue': 'hsl(219, 9%, 45%)',
-      'grayish-blue': 'hsl(220, 14%, 75%)',
-      'light grayish blue': 'hsl(223, 64%, 98%)',
+      'dark-grayish-blue': 'hsl(222, 4%, 48%)',
+      'grayish-blue': 'hsl(230, 4%, 72%)',
+      'light-grayish-blue': 'hsl(223, 64%, 98%)',
       'white': 'hsl(0, 0%, 100%)',
       'black': 'hsl(0, 0%, 0%)'
     },
