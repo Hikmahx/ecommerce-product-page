57cf5ad7b75b05f3d9ad082a68c5c70c24c634f5
Hikmahx <hikmayousuph@gmail.com>
2022-02-20 16:51:18 +0100

    thumbnails state added to context

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -3,16 +3,22 @@ import imgProduct1 from '../assets/image-product-1.jpg'
 import imgProduct2 from '../assets/image-product-2.jpg'
 import imgProduct3 from '../assets/image-product-3.jpg'
 import imgProduct4 from '../assets/image-product-4.jpg'
+import thumbnail1 from '../assets/image-product-1-thumbnail.jpg'
+import thumbnail2 from '../assets/image-product-2-thumbnail.jpg'
+import thumbnail3 from '../assets/image-product-3-thumbnail.jpg'
+import thumbnail4 from '../assets/image-product-4-thumbnail.jpg'
 
 const ProductContext = createContext()
 
 export const ProductProvider = ({children})=>{
 
   const [images, setImages] = useState([imgProduct1, imgProduct2, imgProduct3, imgProduct4])
+  const [thumbnails, setThumbnails] = useState([thumbnail1, thumbnail2, thumbnail3, thumbnail4])
 
   return (
     <ProductContext.Provider value={{
-      images
+      images, 
+      thumbnails
     }}>
       {children}
     </ProductContext.Provider>
