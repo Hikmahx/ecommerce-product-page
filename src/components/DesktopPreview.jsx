7f5b3c69b2f8a41e8b1be09d5f6bec54018e3f47
Hikmahx <hikmayousuph@gmail.com>
2022-02-21 17:53:38 +0100

    function for displaying thumbnail preview

--- src/components/DesktopPreview.jsx
+++ src/components/DesktopPreview.jsx
@@ -3,16 +3,18 @@ import ProductContext from '../context/ProductContext'
 
 const DesktopPreview = () => {
 
-  const {images, thumbnails} = useContext(ProductContext)
+  const {images, thumbnails, thumbnailRef, previewDisplay, previewImg} = useContext(ProductContext)
 
   return (
     <>
     <div className="preview xl:min-w-md max-w-3xl rounded-2xl overflow-hidden">
-      <img src={images[0]} alt="product-preview" />
+      <img src={previewImg} alt="product-preview" />
     </div>
-    <div className="thumbnails flex max-w-3xl justify-between pt-8">
-      {thumbnails.map(img=>(
-        <img className="cursor-pointer w-22 h-22 rounded-xl hover:opacity-50" src={img} alt="thumbnail" />
+    <div ref={thumbnailRef} className="thumbnails flex max-w-3xl justify-between pt-8">
+      {thumbnails.map((img, index)=>(
+        <div className="cursor-pointer w-22 h-22 rounded-xl hover:opacity-80 relative overflow-hidden bg-white">
+          <img key={index} id={index} onClick={previewDisplay} className="w-full" src={img} alt="thumbnail" />
+        </div>
       ))
 
       }
--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react'
+import {createContext, useEffect, useRef, useState} from 'react'
 import imgProduct1 from '../assets/image-product-1.jpg'
 import imgProduct2 from '../assets/image-product-2.jpg'
 import imgProduct3 from '../assets/image-product-3.jpg'
@@ -13,12 +13,46 @@ const ProductContext = createContext()
 export const ProductProvider = ({children})=>{
 
   const [images, setImages] = useState([imgProduct1, imgProduct2, imgProduct3, imgProduct4])
+  const [previewImg, setPreviewImg] = useState(images[0])
   const [thumbnails, setThumbnails] = useState([thumbnail1, thumbnail2, thumbnail3, thumbnail4])
+  const thumbnailRef = useRef(null)
+
+  useEffect(() => {
+
+    // STYLE DEFAULT ACTIVE THUMBNAIL
+    return thumbnailRef.current.childNodes[0].classList.add('border-2', 'border-orange'),
+           thumbnailRef.current.childNodes[0].firstElementChild.classList.add('opacity-50')
+  }, [])
+  
+
+  
+    
+  const previewDisplay = (e)=>{
+
+    // CHANGE PREVIEW IMG ON CLICK
+    setPreviewImg(images[e.target.id])   
+
+    // REMOVE STYLE FROM INACITVE THUMBNAIL
+    let thumbImgs = e.target.parentElement.parentElement.childNodes
+    thumbImgs.forEach(thumbImg => {
+      let img = thumbImg.firstElementChild
+      thumbImg.classList.remove('border-2', 'border-orange')
+      img.classList.remove('opacity-50')
+    });
+
+    // STYLE ACITVE THUMBNAIL
+    e.target.classList.add('opacity-50')
+    e.target.parentElement.classList.add('border-2', 'border-orange')
+  }
+
 
   return (
     <ProductContext.Provider value={{
       images, 
-      thumbnails
+      thumbnails, 
+      thumbnailRef, 
+      previewImg,
+      previewDisplay
     }}>
       {children}
     </ProductContext.Provider>
