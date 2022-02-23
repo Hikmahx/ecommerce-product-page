01fd4e827736e58e606e43b6b18cffe86c6a807b
Hikmahx <hikmayousuph@gmail.com>
2022-02-23 20:45:23 +0100

    hover effect added to btns, min and max attribute added to input

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -31,13 +31,13 @@ const ProductDetails = () => {
       </div>
     </div>
     <div className="sm:flex lg:mt-8 w-full">
-      <div className="quantity-container w-full bg-light-grayish-blue rounded-lg h-14 mb-4 flex items-center justify-between px-6 lg:px-3 font-bold sm:mr-3 lg:mr-5 lg:flex-1">
-        <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl">-</button>
-        <input onChange={quantity} className="quantity focus:outline-none w-1/6 text-dark-blue bg-light-grayish-blue font-bold flex text-center" type="number" name="" id="" value={quantityCount} />
-        <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl">+</button>
+      <div className="quantity-container w-full bg-light-grayish-blue rounded-lg h-14 mb-4 flex items-center justify-between px-6 lg:px-3 font-bold sm:mr-3 lg:mr-5 lg:w-1/3">
+        <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl hover:opacity-60">-</button>
+        <input min={0} max={100} onChange={quantity} className="quantity focus:outline-none text-dark-blue bg-light-grayish-blue font-bold flex text-center w-full" type="number" name="quantity" value={quantityCount} />
+        <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl hover:opacity-60">+</button>
       </div>
 
-      <button className="cart w-full h-14 bg-orange rounded-lg lg:rounded-xl mb-2 shadow-orange-shadow shadow-2xl text-white flex items-center justify-center lg:w-3/5">
+      <button className="cart w-full h-14 bg-orange rounded-lg lg:rounded-xl mb-2 shadow-orange-shadow shadow-2xl text-white flex items-center justify-center lg:w-3/5 hover:opacity-60">
         <i className='cursor-pointer text-white text-xl leading-0 pr-3'>
           <ion-icon name='cart-outline'></ion-icon>
         </i>
