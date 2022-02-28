38f22cce4f50c0e471b50600c02a90be9b07191b
Hikmahx <hikmayousuph@gmail.com>
2022-02-28 19:48:59 +0100

    refs and click event for adding to cart

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -3,14 +3,14 @@ import ProductContext from '../context/ProductContext'
 
 const ProductDetails = () => {
   
-  const {quantity, setQuantityCount, quantityCount} = useContext(ProductContext)
+  const {quantity, setQuantityCount, quantityCount, productTitleRef, productPriceRef, productQuantityRef, addCart} = useContext(ProductContext)
 
   return (
     <>
     <h2 className="company uppercase text-orange font-bold text-sm sm:text-md tracking-wider pb-3 sm:pb-5">
       sneaker company
     </h2>
-    <h3 className="product capitalize text-very-dark-blue font-bold text-3xl sm:text-4xl sm:leading-none pb-3">
+    <h3 ref={productTitleRef} className="product capitalize text-very-dark-blue font-bold text-3xl sm:text-4xl sm:leading-none pb-3">
       fall limited edition <span className="block lg:mt-1">sneakers</span> 
     </h3>
     <p className="text-dark-grayish-blue pb-6 lg:py-7 lg:leading-6">
@@ -21,7 +21,7 @@ const ProductDetails = () => {
     </p>
     <div className="amount font-bold flex items-center justify-between lg:flex-col lg:items-start mb-6">
       <div className="discount-price items-center flex">
-        <div className="price text-3xl">$125.00</div>
+        <div ref={productPriceRef} className="price text-3xl">$125.00</div>
         <div className="discount text-orange bg-pale-orange w-max px-2 rounded mx-5 h-6">
           50%
         </div>
@@ -33,11 +33,11 @@ const ProductDetails = () => {
     <div className="sm:flex lg:mt-8 w-full">
       <div className="quantity-container w-full bg-light-grayish-blue rounded-lg h-14 mb-4 flex items-center justify-between px-6 lg:px-3 font-bold sm:mr-3 lg:mr-5 lg:w-1/3">
         <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl hover:opacity-60">-</button>
-        <input min={0} max={100} onChange={quantity} className="quantity focus:outline-none text-dark-blue bg-light-grayish-blue font-bold flex text-center w-full" type="number" name="quantity" value={quantityCount} />
+        <input ref={productQuantityRef} min={0} max={100} onChange={quantity} className="quantity focus:outline-none text-dark-blue bg-light-grayish-blue font-bold flex text-center w-full" type="number" name="quantity" value={quantityCount} />
         <button onClick={quantity} className="text-orange text-2xl leading-none font-bold mb-1 lg:mb-2 lg:text-3xl hover:opacity-60">+</button>
       </div>
 
-      <button className="cart w-full h-14 bg-orange rounded-lg lg:rounded-xl mb-2 shadow-orange-shadow shadow-2xl text-white flex items-center justify-center lg:w-3/5 hover:opacity-60">
+      <button onClick={addCart} className="cart w-full h-14 bg-orange rounded-lg lg:rounded-xl mb-2 shadow-orange-shadow shadow-2xl text-white flex items-center justify-center lg:w-3/5 hover:opacity-60">
         <i className='cursor-pointer text-white text-xl leading-0 pr-3'>
           <ion-icon name='cart-outline'></ion-icon>
         </i>
