eeff5e6c8ee85018bae01c1e7987abbc67592ea8
Hikmahx <hikmayousuph@gmail.com>
2022-03-01 19:51:20 +0100

    attribution added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Attribution from './components/Attribution';
 import Header from './components/Header'
 import Product from './components/Product'
 import { ProductProvider } from './context/ProductContext';
@@ -5,9 +6,10 @@ import { ProductProvider } from './context/ProductContext';
 function App() {
   return (
     <ProductProvider>
-      <div className='App font-kumbh-sans min-h-screen'>
+      <div className='App font-kumbh-sans min-h-screen relative'>
         <Header />
         <Product />
+        <Attribution />
       </div>
     </ProductProvider>
   );
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Attribution = () => {
+  return (
+    <div className="attribution absolute pb-6 text-xs text-grayish-blue mx-auto flex justify-center w-full" role="contentinfo">
+      <div>Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" className='text-dark-grayish-blue hover:text-orange'> Frontend Mentor</a></div>
+      <div>. Coded by <a href="https://github.com/Hikmahx" className='text-dark-grayish-blue hover:text-orange'> Hikmah</a>.</div>
+    </div>
+
+  )
+}
+
+export default Attribution
